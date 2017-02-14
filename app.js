baed813fe8de47ee14d4ad785a35bf1855752a88
Kevin Noll <kevinnoll90@gmail.com>
2017-02-14 21:41:22 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+
+var port = 5000;
+
+app.get('/', function(req,res) {
+  res.send('Hello World');
+});
+
+app.get('/test', function(req,res) {
+  res.send('Hello Test');
+});
+
+app.listen(port, function (err) {
+  console.log('running...');
+});
