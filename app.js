299a6e1d418942e07f5aa9828f981303f66bf7b3
Kevin Noll <kevinnoll90@gmail.com>
2017-02-15 11:39:53 +0100

    new template

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
 app.get('/', function(req,res) {
   res.send('Hello World');
 });
