e4a3c40d3fd5267cec54223d2f359481bec9f108
Kevin Noll <kevinnoll90@gmail.com>
2017-02-16 12:10:37 +0100

    jshint + jscs

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.use(express.static('src/views'))
 
-app.get('/', function(req,res) {
-  res.send('Hello World');
+app.get('/', function (req, res) {
+    res.send('Hello World');
 });
 
-app.get('/test', function(req,res) {
-  res.send('Hello Test');
+app.get('/test', function (req, res) {
+    res.send('Hello Test');
 });
 
 app.listen(process.env.PORT || 5000, function (err) {
-  console.log('running...');
+    console.log('running...');
 });
