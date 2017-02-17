415c2eae6735fc5a884b70e4ac600fbfd0886590
Kevin Noll <kevinnoll90@gmail.com>
2017-02-17 10:46:54 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,26 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    jscs = require('gulp-jscs'),
+    jsFiles = ['*.js', 'src/**/*.js'];
 
 gulp.task('style', function () {
-    gulp.src();
+    return gulp.src(jsFiles)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {
+            verbose: true
+        }))
+        .pipe(jscs());
+});
+
+gulp.task('inject', function() {
+    var wiredep = require('wiredep').stream,
+        options = {
+            bowerJson : require('./bower.json'),
+            directory : './public/lib',
+            ignorePath : '../../public'
+        };
+
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(gulp.dest('./src/views'));
 });
