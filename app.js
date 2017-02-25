0376454eb27f756477632989d10150d8faa4cd28
Kevin Noll <kevinnoll90@gmail.com>
2017-02-25 14:56:34 +0100

    new branch for angular2

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 var express = require('express');
-
 var app = express();
+var path = require('path');
 
+
+//require('./node_modules/ng2f-server/expressConfig')(app);
+
+//require('./node_modules/ng2f-server/passport')();
+
+//require('./node_modules/ng2f-server/routes')(app);
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.use(express.static('.'));
 app.use(express.static('public'));
-app.use(express.static('src/views'));
 
-app.get('/', function (req, res) {
+
+/*app.get('/', function (req, res) {
     res.send('Hello World');
 });
 
 app.get('/test', function (req, res) {
     res.send('Hello Test');
 });
-
+*/
 app.listen(process.env.PORT || 5000, function (err) {
     console.log('running...');
 });
