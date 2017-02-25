d415a0bc0e2fba4ecc8283afa7d378f72c5270ab
Kevin Noll <kevinnoll90@gmail.com>
2017-02-25 12:20:55 +0100

    nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     jscs = require('gulp-jscs'),
+    nodemon = require('gulp-nodemon'),
     jsFiles = ['*.js', 'src/**/*.js'];
 
 gulp.task('style', function () {
@@ -29,4 +30,18 @@ gulp.task('inject', function() {
         .pipe(wiredep(options))
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'], function () {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 5000
+        },
+        watch: jsFiles
+    };
+    return nodemon(options).on('restart', function () {
+        console.log('Restarting...');
+    });
 });
